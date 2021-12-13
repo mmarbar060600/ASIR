de7ca9b06e7e0360ca438b34c6235f72948aab5f
manuel <manumarti2000@gmail.com>
2021-12-13 10:28:59 +0100

    Ejercicio de ficheros con escribir y leer hecho

--- primer trimestre/ficheros/ficheros.php
+++ primer trimestre/ficheros/ficheros.php
@@ -1,4 +1,8 @@
 <?php
+$datosrecogidos=$_POST["texto1"];
+$opcion=$_POST["opcion"];
+/*
+//ejemplo del ejercicio sin recoger los datos en el html
 echo readfile("pruebafichero.txt");
 echo "<br>";
 echo filesize("pruebafichero.txt");
@@ -13,6 +17,34 @@ $pesofichero= filesize("pruebafichero.txt");
 //leo el fichero junto con el peso 
 echo fread($fichero,$pesofichero);
 fclose($fichero);
-
+echo "<br>";
+//escribir
+$fichero= fopen("pruebafichero.txt","w") or die("Er migue de aeronautica");
+$nuevotexto="que te gusta tu padre";
+$nuevotexto2=$datosrecogidos;
+fwrite($fichero,$nuevotexto);
+fclose($fichero);
+*/
+switch ($opcion) {
+    case '1':
+        $fichero= fopen("pruebafichero.txt","w") or die("Er migue de aeronautica");
+        fwrite($fichero,$datosrecogidos);
+        fclose($fichero);
+        echo "Fichero editado para leerlo volver atras";
+        break;
+    case '2':
+        $fichero= fopen("pruebafichero.txt","r") or die("Er migue de aeronautica");
+            //creo una variable con el peso
+        $pesofichero= filesize("pruebafichero.txt");
+                //leo el fichero junto con el peso 
+            echo fread($fichero,$pesofichero);
+        fclose($fichero);
+            echo "<br>";
+        break;
+    
+        default:
+         echo "Opcion no valida";
+            break;
+}
 ?>
 
