eba9dfc3bb5b347bf8d25874c38d33379cbd1e60
manuel <manumarti2000@gmail.com>
2021-12-13 09:47:41 +0100

    cambio nuevo al php

--- primer trimestre/ficheros/ficheros.php
+++ primer trimestre/ficheros/ficheros.php
@@ -8,8 +8,11 @@ echo "<br>";
 //Apertura y lectura de un fichero
 
 $fichero= fopen("pruebafichero.txt","r") or die("Er migue de aeronautica");
+//creo una variable con el peso
 $pesofichero= filesize("pruebafichero.txt");
- echo fread($fichero,$pesofichero);
+//leo el fichero junto con el peso 
+echo fread($fichero,$pesofichero);
 fclose($fichero);
+
 ?>
 
