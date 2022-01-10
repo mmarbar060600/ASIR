2d77a1f42446016529bd5ab475f71800446400c9
manuel <manumarti2000@gmail.com>
2022-01-10 09:24:42 +0100

    desordenar array

--- segundo trimestre/Repasito.php
+++ segundo trimestre/Repasito.php
@@ -5,6 +5,17 @@ $pi=pi();
 echo "numero pi: ". $pi."<br>";
 define('GRAVEDAD',9.8);
 echo "Constante de la gravedad: ".GRAVEDAD;
+echo "<br>";
+$numeros= array(2,4,8,7,4,10,GRAVEDAD);
+print_r($numeros);
+$cantidad_numeros=count($numeros);
+$desordena=shuffle($numeros);
+echo "<br>";
+print_r("Cuenta numeros: ".$cantidad_numeros);
+echo "<br>";
+print_r($numeros);
+echo "<br>";
+print_r("desordena ".$desordena);
 /*
 $manule="cafelaso de arvellana";
 
