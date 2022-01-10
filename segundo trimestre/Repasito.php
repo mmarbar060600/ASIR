c448c0417e0f688a41c87a0533885d6eabe0ca91
manuel <manumarti2000@gmail.com>
2022-01-10 09:01:00 +0100

    Primeros cambios del repaso

--- segundo trimestre/Repasito.php
+++ segundo trimestre/Repasito.php
@@ -1,3 +1,30 @@
 <?php
+$manule="cafelaso de arvellana";
 
+$migue=0;
+
+$pako=true;
+
+$dato1=$_POST["datito1"];
+$dato2=$_GET["datito2"];
+#condicionales
+if (condition) {
+    # code...
+}
+switch ($variable) {
+    case 'value':
+        # code...
+        break;
+    
+    default:
+        # code...
+        break;
+}
+#BUCLES
+while ($a <= 10) {
+    # code...
+}
+for ($i=0; $i < ; $i++) { 
+    # code...
+}
 ?>
