722412721f7d6fafb78ce67d421264fd1b0ccb0d
manuel <manumarti2000@gmail.com>
2022-01-10 09:36:25 +0100

    bucles for y for each

--- segundo trimestre/Repasito.php
+++ segundo trimestre/Repasito.php
@@ -8,14 +8,27 @@ echo "Constante de la gravedad: ".GRAVEDAD;
 echo "<br>";
 $numeros= array(2,4,8,7,4,10,GRAVEDAD);
 print_r($numeros);
+//contar
 $cantidad_numeros=count($numeros);
+//desordenar
 $desordena=shuffle($numeros);
+//mostrar por pantalla
 echo "<br>";
 print_r("Cuenta numeros: ".$cantidad_numeros);
 echo "<br>";
 print_r($numeros);
 echo "<br>";
 print_r("desordena ".$desordena);
+echo "<br>";
+//bucle recorrer
+for ($i=0; $i < $cantidad_numeros ; $i++) { 
+    echo "Valor de la posicion ".$i.": ".$numeros[$i]."<br>";
+}
+//ejemplo for each
+echo "<br> For each <br>";
+foreach ($numeros as $numerito) {
+    echo $numerito."<br>";
+}
 /*
 $manule="cafelaso de arvellana";
 
