eb3e77e4027e7adc0567cc84a019ca9449944ce1
mmarbar060600 <82811505+mmarbar060600@users.noreply.github.com>
2022-01-26 12:30:37 +0100

    cambios en el codigo

--- segundo trimestre/ejercicio puntuable enero/Funciones.php
+++ segundo trimestre/ejercicio puntuable enero/Funciones.php
@@ -1,27 +1,7 @@
 <?php
 //recoger datos del otro php
 include 'datosvuelos.php';
-    //mostrar
-    //     $m1= $entrada["m1"];
-    //     $m2= $entrada["m2"];
-    //     $m3= $entrada["m3"];
-    //     $m4= $entrada["m4"];
-    // //estadisticas aero
-    //     $e1= $entrada["e1"];
-    //     $e2= $entrada["e2"];
-    //     $e3= $entrada["e3"];
-    //     $e4= $entrada["e4"];
-    //     $e5= $entrada["e5"];
-    // //estadisticas ciudades
-    //     $c1= $entrada["c1"];
-    //     $c2= $entrada["c2"];
-    //     $c3= $entrada["c3"];
-    //     $c4= $entrada["c4"];
-    // //estadisticas avion
-    //     $es1= $entrada["es1"];
-    //     $es2= $entrada["es2"];
-    //     $es3= $entrada["es3"];
-    //     $es4= $entrada["es4"];
+//creacion de funciones
 function mediapasa($vuelopasa,$operando1)
 {
 $contador=0;
